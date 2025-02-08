8816518542693c6ca60f53230b17857cf2a89309
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2025-02-09 05:46:55 +0800

    typing

--- bsif/utils/notifier.py
+++ bsif/utils/notifier.py
@@ -1,11 +1,11 @@
 from traceback import print_exception
-from typing import Callable, List
+from typing import Any, Callable, List, Unpack
 
-class Notifier:
+class Notifier[*AT]:
 	def __init__(self):
 		self.__handlers: List[Callable] = []
 
-	def add_handler(self, handler: Callable):
+	def add_handler(self, handler: Callable[[Unpack[AT]], Any]):
 		if not callable(handler): raise TypeError("Not callable")
 		self.__handlers.append(handler)
 
@@ -15,7 +15,7 @@ class Notifier:
 	def remove_all_handlers(self):
 		self.__handlers = []
 
-	def trigger(self, *args):
+	def trigger(self, *args: Unpack[AT]):
 		for item in self.__handlers:
 			try: item(*args)
 			except Exception as e: print_exception(e)
