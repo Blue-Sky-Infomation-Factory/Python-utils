4fdb0e475ce2fc7635e8762623030b28bcbc569f
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2025-09-26 11:19:13 +0800

    Update typedarray.py

--- bsif_utils/typedarray.py
+++ bsif_utils/typedarray.py
@@ -1,7 +1,10 @@
 from abc import ABC
 from math import log2
+from multiprocessing.shared_memory import SharedMemory
 from struct import calcsize, pack, pack_into, unpack, unpack_from
 from typing import Iterator, List, Tuple
+from array import array
+from inspect import BufferFlags
 
 from .abstractmeta import AbstractMeta, realabstractmethod
 
@@ -14,21 +17,28 @@ class TypedArrayMeta(ABC, AbstractMeta):
 	@realabstractmethod
 	def format(self) -> Tuple[str, int, int]: ...
 class TypedArray(metaclass=TypedArrayMeta):
-	def __init__(self, array: bytearray | bytes | int | List | Tuple) -> None:
+	def __init__(self, buffer: bytearray | bytes | int | List | Tuple | array | memoryview | SharedMemory) -> None:
 		[format, offset, byte_length] = self.__class__.format
-		if isinstance(array, (bytearray, bytes)):
-			if len(array) % byte_length: raise ValueError(f"Length of array must be a multiple of {byte_length}.")
-			self.__bytes = array
-		elif isinstance(array, int):
-			self.__bytes = bytearray(array << offset)
-		elif isinstance(array, (list, tuple)):
-			self.__bytes = pack(f"{len(array)}{format}", *array)
-		else: raise TypeError("Argument 'array' must be one of the following types: (bytearray | bytes | int | List | Tuple).")
-	
+		if isinstance(buffer, (memoryview, SharedMemory)):
+			if isinstance(buffer, SharedMemory): buffer = buffer.buf
+			if buffer.nbytes % byte_length: raise ValueError(f"The length in bytes of buffer must be a multiple of {byte_length}.")
+			self.__buffer = buffer
+		elif isinstance(buffer, (bytearray, bytes)):
+			if len(buffer) % byte_length: raise ValueError(f"The length in bytes of buffer must be a multiple of {byte_length}.")
+			self.__buffer = memoryview(buffer)
+		elif isinstance(buffer, array):
+			if buffer.itemsize != byte_length: raise ValueError(f"The itemsize of array must be {byte_length}.")
+			self.__buffer = memoryview(buffer)
+		elif isinstance(buffer, int):
+			self.__buffer = memoryview(bytearray(buffer << offset))
+		elif isinstance(buffer, (list, tuple)):
+			self.__buffer = memoryview(pack(f"{len(buffer)}{format}", *buffer))
+		else: raise TypeError("Argument 'array' must be one of the following types: (bytearray | bytes | int | List | Tuple | array | memoryview | SharedMemory).")
+
 	def __init_subclass__(cls):
 		[format, offset, _] = cls.format
-		cls.__getitem__ = lambda self, index: unpack_from(format, self.__bytes, index << offset)[0]
-		cls.__setitem__ = lambda self, index, value: pack_into(format, self.__bytes, index << offset, value)
+		cls.__getitem__ = lambda self, index: unpack_from(format, self.__buffer, index << offset)[0]
+		cls.__setitem__ = lambda self, index, value: pack_into(format, self.__buffer, index << offset, value)
 
 	@classmethod
 	def from_iterator(cls, length: int, iterator: Iterator):
@@ -38,23 +48,23 @@ class TypedArray(metaclass=TypedArrayMeta):
 		return cls(pack(f"{length}{format}", *iterator))
 
 	@property
-	def is_readonly(self): return isinstance(self.__bytes, bytes)
+	def is_readonly(self): return self.__buffer.readonly
 	@property
-	def byte_length(self): return len(self.__bytes)
+	def byte_length(self): return self.__buffer.nbytes
 	
 	def __getitem__(self, index: int):
 		[format, offset, _] = self.__class__.format
-		return unpack_from(format, self.__bytes, index << offset)[0]
-	def __setitem__(self, index: int, value: int):
+		return unpack_from(format, self.__buffer, index << offset)[0]
+	def __setitem__(self, index: int, value: int | float):
 		[format, offset, _] = self.__class__.format
-		pack_into(format, self.__bytes, index << offset, value)
+		pack_into(format, self.__buffer, index << offset, value)
 	def __iter__(self):
 		for i in range(len(self)): yield self[i]
-	def __len__(self): return len(self.__bytes) >> self.__class__.format[1]
+	def __len__(self): return self.__buffer.nbytes >> self.__class__.format[1]
 	def to_tuple(self):
-		buffer = self.__bytes
+		buffer = self.__buffer
 		[format, offset, _] = self.__class__.format
-		return unpack(f"{len(buffer) >> offset}{format}", buffer)
+		return unpack(f"{buffer.nbytes >> offset}{format}", buffer)
 	def __repr__(self):
 		length = len(self)
 		temp = []
@@ -64,6 +74,9 @@ class TypedArray(metaclass=TypedArrayMeta):
 		else:
 			for i in range(length): temp.append(str(self[i]))
 		return f"{self.__class__.__name__}({length})[{', '.join(temp)}]"
+	def __buffer__(self, flags: int):
+		if BufferFlags.WRITABLE & flags and self.__buffer.readonly: raise BufferError("Internal buffer is not writable.")
+		return self.__buffer
 
 class Uint32ArrayMeta(TypedArrayMeta):
 	__format = format_info("I")
@@ -75,3 +88,8 @@ class Float64ArrayMeta(TypedArrayMeta):
 	@property
 	def format(self): return self.__format
 class Float64Array(TypedArray, metaclass=Float64ArrayMeta): pass
+class Float32ArrayMeta(TypedArrayMeta):
+	__format = format_info("f")
+	@property
+	def format(self): return self.__format
+class Float32Array(TypedArray, metaclass=Float32ArrayMeta): pass
