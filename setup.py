caad281a6d7a8e6aaa23cb5a44fd1c49dc9be82b
Fina <42113148+BSPR0002@users.noreply.github.com>
2026-03-19 08:18:33 +0800

    Bump version and simplify setup packaging
    Update setup.py: bump package version to 1.0.1, remove find_packages import and use an explicit packages list (['bsif_utils']), and replace the build comment with a sdist command. These changes make packaging explicit and update the recommended build step.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('README.md', 'r') as file:
     readme = file.read()
 
 setup(
 	name="bsif-utils",
-	version="1.0.0",
+	version="1.0.1",
 	author="Blue Sky Infomation Factory",
 	description="Utils package.",
 	long_description=readme,
 	long_description_content_type="text/markdown",
-	packages=find_packages(),
+	packages=["bsif_utils"],
 	package_data={},
 	license="BSD 3-Clause License",
 	python_requires=">=3.11",
@@ -21,4 +21,4 @@ setup(
 	url="https://github.com/Blue-Sky-Infomation-Factory/Python-utils"
 )
 
-# python -m build -w
+# python setup.py sdist
